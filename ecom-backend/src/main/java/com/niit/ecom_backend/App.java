21cff4b4e46101e63d40c4158da606ab9840193a
pavanpoojari <pavan.poojary035@gmail.com>
2016-12-05 01:28:47 -0800

    THE FIRST COMMIT

---
+++ ecom-backend/src/main/java/com/niit/ecom_backend/App.java
@@ -0,0 +1,13 @@
+package com.niit.ecom_backend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
