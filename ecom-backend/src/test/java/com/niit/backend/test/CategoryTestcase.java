f428951e28e1830104befb728031b09f8da4345d
pavan <pavan.poojary035@gmail.com>
2017-01-15 21:24:22 +0530

    JAN 9th : ADDING TEST CASE TO BACKEND.

---
+++ ecom-backend/src/test/java/com/niit/backend/test/CategoryTestcase.java
@@ -0,0 +1,14 @@
+package com.niit.backend.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CategoryTestcase {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ ecom-backend/src/test/java/com/niit/backend/test/UserTestcase.java
@@ -0,0 +1,14 @@
+package com.niit.backend.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UserTestcase {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
